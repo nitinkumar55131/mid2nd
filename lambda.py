f982f72dcf00fbaa8c4a874df3a7ad627cccfa3b
nitinkumar55131 <49191046+nitinkumar55131@users.noreply.github.com>
2019-04-02 15:36:04 +0530

    Add files via upload
    solutions o

---
+++ lambda.py
@@ -0,0 +1,5 @@
+add=lambda a,b:a+b
+A=[12,23,4,3]
+f=[i+i for i in range(10)]
+print(f)
+
---
+++ second.py
@@ -0,0 +1,5 @@
+#count()-it is used for count the number of characters in string
+a='hello'
+h=a.count('l')
+print(h)
+
